acdcff415198a6034da7a2c424ccd163ff6ccc82
varshavskiy_png <timikinvarshav05@gmail.com>
2024-11-05 19:16:54 +0200

    Added auth template, configured paths, added page's fillings

--- toDoList/todo/models.py
+++ toDoList/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    user_profile = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name= 'Task'
+        verbose_name_plural= 'Tasks'
--- toDoList/todo/views.py
+++ toDoList/todo/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'todo/index.html')
 
+
+def about(request):
+    return render(request, 'todo/about.html')
+
+
+def auth(request):
+    return render(request, 'todo/auth.html')
