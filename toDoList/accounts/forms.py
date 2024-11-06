eab9d424e7816df770ef07f513a30b7421fea5e7
varshavskiy_png <timikinvarshav05@gmail.com>
2024-11-06 23:09:30 +0200

    Registration added

--- toDoList/accounts/forms.py
+++ toDoList/accounts/forms.py
@@ -4,17 +4,29 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'})
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'form-control'}),
+        }
+
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
 
     def clean(self):
         cleaned_data = super().clean()
